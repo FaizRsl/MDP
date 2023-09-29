9179e8fc329a4bbe6427b10fa60d6f67dbf7b5ef
Raghav Rajendran Nair <raghavrnair@gmail.com>
2023-09-29 14:43:51 +0800

    Update taskA2.py

--- imagerec/taskA2.py
+++ imagerec/taskA2.py
@@ -120,10 +120,15 @@ def draw_own_bbox(img,x1,y1,x2,y2,label,color=(36,255,12),text_color=(0,0,0)):
     # Print the text  
     img = cv2.rectangle(img, (x1, y1 - 20), (x1 + w, y1), color, -1)
     img = cv2.putText(img, label, (x1, y1 - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.6, text_color, 1)
+    
     # Save the annotated image
     cv2.imwrite(f"own_results/annotated_image_{label}_{rand}.jpg", img)
+   
+    # Display the annotated image
+    cv2.imshow("Annotated Image", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-    ##Display the image
 
 
 def predict_image(image, model):
@@ -142,185 +147,185 @@ def predict_image(image, model):
     -------
     str - predicted label
     """
-    try:
-        # Load the image
-        img = Image.open(os.path.join('uploads', image))
-
-        # Predict the image using the model
-        results = model(img)
-
-        # Images with predicted bounding boxes are saved in the runs folder
-        results.save('runs')
-
-        # Convert the results to a pandas dataframe and calculate the height and width of the bounding box and the area of the bounding box
-        df_results = results.pandas().xyxy[0]
-        df_results['bboxHt'] = df_results['ymax'] - df_results['ymin']
-        df_results['bboxWt'] = df_results['xmax'] - df_results['xmin']
-        df_results['bboxArea'] = df_results['bboxHt'] * df_results['bboxWt']
-
-        # Label with largest bbox height will be last
-        df_results = df_results.sort_values('bboxArea', ascending=False)
-
-        # Filter out Bullseye
-        pred_list = df_results 
-        pred_list = pred_list[pred_list['name'] != 'Bullseye']
-        
-        # Initialize prediction to NA
-        pred = 'NA'
-
-        # Ignore Bullseye unless they are the only image detected and select the last label in the list (the last label will be the one with the largest bbox height)
-        if len(pred_list) == 1:
-            if pred_list.iloc[0]['name'] != 'Bullseye':
-                pred = pred_list.iloc[0]
-
-        # If more than 1 label is detected
-        elif len(pred_list) > 1:
-
-            # More than 1 Symbol detected, filter by confidence and area
-            pred_shortlist = []
-            current_area = pred_list.iloc[0]['bboxArea']
-            # For each prediction, check if the confidence is greater than 0.5 and if the area is greater than 80% of the current area or 60% if the prediction is 'One'
-            for _, row in pred_list.iterrows():
-                if row['name'] != 'Bullseye' and row['confidence'] > 0.5 and ((current_area * 0.8 <= row['bboxArea']) or (row['name'] == 'One' and current_area * 0.6 <= row['bboxArea'])):
-                    # Add the prediction to the shortlist
-                    pred_shortlist.append(row)
-                    # Update the current area to the area of the prediction
-                    current_area = row['bboxArea']
-            
-            # If only 1 prediction remains after filtering by confidence and area
-            if len(pred_shortlist) == 1:
-                # Choose that prediction
-                pred = pred_shortlist[0]
-
-            # If multiple predictions remain after filtering by confidence and area
-            # else:
-            #     # Use signal of {signal} to filter further 
-                
-            #     # Sort the predictions by xmin
-            #     pred_shortlist.sort(key=lambda x: x['xmin'])
-
-            #     # If signal is 'L', choose the first prediction in the list, i.e. leftmost in the image
-            #     if signal == 'L':
-            #         pred = pred_shortlist[0]
-                
-            #     # If signal is 'R', choose the last prediction in the list, i.e. rightmost in the image
-            #     elif signal == 'R':
-            #         pred = pred_shortlist[-1]
-                
-            #     # If signal is 'C', choose the prediction that is central in the image
-            #     else:
-            #         # Loop through the predictions shortlist
-            #         for i in range(len(pred_shortlist)):
-            #             # If the xmin of the prediction is between 250 and 774, i.e. the center of the image, choose that prediction
-            #             if pred_shortlist[i]['xmin'] > 250 and pred_shortlist[i]['xmin'] < 774:
-            #                 pred = pred_shortlist[i]
-            #                 break
-                    
-            #         # If no prediction is central, choose the one with the largest area
-            #         if isinstance(pred,str):
-            #             # Choosing one with largest area if none are central
-            #             pred_shortlist.sort(key=lambda x: x['bboxArea']) 
-            #             pred = pred_shortlist[-1]
-        
-        # Draw the bounding box on the image
-        if not isinstance(pred,str):
-            draw_own_bbox(np.array(img), pred['xmin'], pred['ymin'], pred['xmax'], pred['ymax'], pred['name'])
-
-        name_to_id = {
-            "NA": 'NA',
-            "Bullseye": 10,
-            "One": 11,
-            "Two": 12,
-            "Three": 13,
-            "Four": 14,
-            "Five": 15,
-            "Six": 16,
-            "Seven": 17,
-            "Eight": 18,
-            "Nine": 19,
-            "A": 20,
-            "B": 21,
-            "C": 22,
-            "D": 23,
-            "E": 24,
-            "F": 25,
-            "G": 26,
-            "H": 27,
-            "S": 28,
-            "T": 29,
-            "U": 30,
-            "V": 31,
-            "W": 32,
-            "X": 33,
-            "Y": 34,
-            "Z": 35,
-            "Up": 36,
-            "Down": 37,
-            "Right": 38,
-            "Left": 39,
-            "Up Arrow": 36,
-            "Down Arrow": 37,
-            "Right Arrow": 38,
-            "Left Arrow": 39,
-            "Stop": 40
-        }
-        # If pred is not a string, i.e. a prediction was made and pred is not 'NA'
-        if not isinstance(pred,str):
-            image_id = str(name_to_id[pred['name']])
-        else:
-            image_id = 'NA'
-        print(f"Final result: {image_id}")
-        return image_id
-    # If some error happened, we just return 'NA' so that the inference loop is closed
-    except:
-        print(f"Final result: NA")
-        return 'NA'
-
-def predict_image_week_9(image, model):
     # Load the image
     img = Image.open(os.path.join('uploads', image))
-    # Run inference
+
+    # Predict the image using the model
     results = model(img)
-    # Save the results
+
+    # Images with predicted bounding boxes are saved in the runs folder
     results.save('runs')
-    # Convert the results to a dataframe
+
+    # Convert the results to a pandas dataframe and calculate the height and width of the bounding box and the area of the bounding box
     df_results = results.pandas().xyxy[0]
-    # Calculate the height and width of the bounding box and the area of the bounding box
     df_results['bboxHt'] = df_results['ymax'] - df_results['ymin']
     df_results['bboxWt'] = df_results['xmax'] - df_results['xmin']
     df_results['bboxArea'] = df_results['bboxHt'] * df_results['bboxWt']
 
     # Label with largest bbox height will be last
     df_results = df_results.sort_values('bboxArea', ascending=False)
-    pred_list = df_results 
-    pred = 'NA'
-    # If prediction list is not empty
-    if pred_list.size != 0:
-        # Go through the predictions, and choose the first one with confidence > 0.5
-        for _, row in pred_list.iterrows():
-            if row['name'] != 'Bullseye' and row['confidence'] > 0.5:
-                pred = row    
-                break
 
-        # Draw the bounding box on the image 
-        if not isinstance(pred,str):
-            draw_own_bbox(np.array(img), pred['xmin'], pred['ymin'], pred['xmax'], pred['ymax'], pred['name'])
+    # Filter out Bullseye
+    pred_list = df_results 
+    pred_list = pred_list[pred_list['name'] != 'Bullseye']
+    
+    # Initialize prediction to NA
+    pred = 'NA'
+
+    # Ignore Bullseye unless they are the only image detected and select the last label in the list (the last label will be the one with the largest bbox height)
+    if len(pred_list) == 1:
+        if pred_list.iloc[0]['name'] != 'Bullseye':
+            pred = pred_list.iloc[0]
+
+    # If more than 1 label is detected
+    elif len(pred_list) > 1:
+
+        # More than 1 Symbol detected, filter by confidence and area
+        pred_shortlist = []
+        current_area = pred_list.iloc[0]['bboxArea']
+        # For each prediction, check if the confidence is greater than 0.5 and if the area is greater than 80% of the current area or 60% if the prediction is 'One'
+        for _, row in pred_list.iterrows():
+            if row['name'] != 'Bullseye' and row['confidence'] > 0.5 and ((current_area * 0.8 <= row['bboxArea']) or (row['name'] == 'One' and current_area * 0.6 <= row['bboxArea'])):
+                # Add the prediction to the shortlist
+                pred_shortlist.append(row)
+                # Update the current area to the area of the prediction
+                current_area = row['bboxArea']
         
-    # Dictionary is shorter as only two symbols, left and right are needed
-    name_to_id = {
-        "NA": 'NA',
-        "Bullseye": 10,
-        "Right": 38,
-        "Left": 39,
-        "Right Arrow": 38,
-        "Left Arrow": 39,
-    }
-    # Return the image id
+        # If only 1 prediction remains after filtering by confidence and area
+        if len(pred_shortlist) == 1:
+            # Choose that prediction
+            pred = pred_shortlist[0]
+
+        # If multiple predictions remain after filtering by confidence and area
+        # else:
+        #     # Use signal of {signal} to filter further 
+            
+        #     # Sort the predictions by xmin
+        #     pred_shortlist.sort(key=lambda x: x['xmin'])
+
+        #     # If signal is 'L', choose the first prediction in the list, i.e. leftmost in the image
+        #     if signal == 'L':
+        #         pred = pred_shortlist[0]
+            
+        #     # If signal is 'R', choose the last prediction in the list, i.e. rightmost in the image
+        #     elif signal == 'R':
+        #         pred = pred_shortlist[-1]
+            
+        #     # If signal is 'C', choose the prediction that is central in the image
+        #     else:
+        #         # Loop through the predictions shortlist
+        #         for i in range(len(pred_shortlist)):
+        #             # If the xmin of the prediction is between 250 and 774, i.e. the center of the image, choose that prediction
+        #             if pred_shortlist[i]['xmin'] > 250 and pred_shortlist[i]['xmin'] < 774:
+        #                 pred = pred_shortlist[i]
+        #                 break
+                
+        #         # If no prediction is central, choose the one with the largest area
+        #         if isinstance(pred,str):
+        #             # Choosing one with largest area if none are central
+        #             pred_shortlist.sort(key=lambda x: x['bboxArea']) 
+        #             pred = pred_shortlist[-1]
+    
+    # Draw the bounding box on the image
     if not isinstance(pred,str):
-        image_id = str(name_to_id[pred['name']])
-    else:
-        image_id = 'NA'
-    return image_id
+        draw_own_bbox(np.array(img), pred['xmin'], pred['ymin'], pred['xmax'], pred['ymax'], pred['name'])
+
+
+#     name_to_id = {
+#         "NA": 'NA',
+#         "Bullseye": 10,
+#         "One": 11,
+#         "Two": 12,
+#         "Three": 13,
+#         "Four": 14,
+#         "Five": 15,
+#         "Six": 16,
+#         "Seven": 17,
+#         "Eight": 18,
+#         "Nine": 19,
+#         "A": 20,
+#         "B": 21,
+#         "C": 22,
+#         "D": 23,
+#         "E": 24,
+#         "F": 25,
+#         "G": 26,
+#         "H": 27,
+#         "S": 28,
+#         "T": 29,
+#         "U": 30,
+#         "V": 31,
+#         "W": 32,
+#         "X": 33,
+#         "Y": 34,
+#         "Z": 35,
+#         "Up": 36,
+#         "Down": 37,
+#         "Right": 38,
+#         "Left": 39,
+#         "Up Arrow": 36,
+#         "Down Arrow": 37,
+#         "Right Arrow": 38,
+#         "Left Arrow": 39,
+#         "Stop": 40
+#     }
+#     # If pred is not a string, i.e. a prediction was made and pred is not 'NA'
+#     if not isinstance(pred,str):
+#         image_id = str(name_to_id[pred['name']])
+#     else:
+#         image_id = 'NA'
+#     print(f"Final result: {image_id}")
+#     return image_id
+# # If some error happened, we just return 'NA' so that the inference loop is closed
+# except:
+#     print(f"Final result: NA")
+#     return 'NA'
+
+# def predict_image_week_9(image, model):
+#     # Load the image
+#     img = Image.open(os.path.join('uploads', image))
+#     # Run inference
+#     results = model(img)
+#     # Save the results
+#     results.save('runs')
+#     # Convert the results to a dataframe
+#     df_results = results.pandas().xyxy[0]
+#     # Calculate the height and width of the bounding box and the area of the bounding box
+#     df_results['bboxHt'] = df_results['ymax'] - df_results['ymin']
+#     df_results['bboxWt'] = df_results['xmax'] - df_results['xmin']
+#     df_results['bboxArea'] = df_results['bboxHt'] * df_results['bboxWt']
+
+#     # Label with largest bbox height will be last
+#     df_results = df_results.sort_values('bboxArea', ascending=False)
+#     pred_list = df_results 
+#     pred = 'NA'
+#     # If prediction list is not empty
+#     if pred_list.size != 0:
+#         # Go through the predictions, and choose the first one with confidence > 0.5
+#         for _, row in pred_list.iterrows():
+#             if row['name'] != 'Bullseye' and row['confidence'] > 0.5:
+#                 pred = row    
+#                 break
+
+#         # Draw the bounding box on the image 
+#         if not isinstance(pred,str):
+#             draw_own_bbox(np.array(img), pred['xmin'], pred['ymin'], pred['xmax'], pred['ymax'], pred['name'])
+        
+#     # Dictionary is shorter as only two symbols, left and right are needed
+#     name_to_id = {
+#         "NA": 'NA',
+#         "Bullseye": 10,
+#         "Right": 38,
+#         "Left": 39,
+#         "Right Arrow": 38,
+#         "Left Arrow": 39,
+#     }
+#     # Return the image id
+#     if not isinstance(pred,str):
+#         image_id = str(name_to_id[pred['name']])
+#     else:
+#         image_id = 'NA'
+#     return image_id
 
 
 def stitch_image():
