50e5e36d45a92827b6a787689ee32777672a4933
teg tiwana <82508380+tej172@users.noreply.github.com>
2023-10-26 00:11:08 +0800

    Update main.py

--- ALGO/ALGO_MAIN/AlGO_MDP/main.py
+++ ALGO/ALGO_MAIN/AlGO_MDP/main.py
@@ -1,7 +1,9 @@
 import time
-from algo.algo import MazeSolver 
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+
+
+from algo.algo import MazeSolver
 from model import *
 from helper import command_generator
 
@@ -29,7 +31,12 @@ def path_finding():
     # Get the json data from the request
     content = request.json
 
-    print(f"content is:: {content}\n")
+    print("\n::::Got the obstacles from RPI::::\n")
+    print(f"Recieved Content from RPI is:: {content}\n")
+    # Create an instance of the maze solver class
+    
+
+
 
     # Get the obstacles, big_turn, retrying, robot_x, robot_y, and robot_direction from the json data
     obstacles = content['obstacles']
@@ -37,18 +44,25 @@ def path_finding():
     retrying = content['retrying']
     robot_x, robot_y = content['robot_x'], content['robot_y']
     robot_direction = int(content['robot_dir'])
-
+    
+    print("\n:::CREATING MAZE ON LAPTOP NOW:::")
     # Initialize MazeSolver object with robot size of 20x20, bottom left corner of robot at (1,1), facing north, and whether to use a big turn or not.
     maze_solver = MazeSolver(20, 20, robot_x, robot_y, robot_direction, big_turn=None)
-
+    
     # Add each obstacle into the MazeSolver. Each obstacle is defined by its x,y positions, its direction, and its id
     for ob in obstacles:
         maze_solver.add_obstacle(ob['x'], ob['y'], ob['d'], ob['id'])
 
+    print("::::Running through A*Star Algo::::\n")
     start = time.time()
     # Get shortest path
     optimal_path, distance = maze_solver.get_optimal_order_dp(retrying=retrying)
     end=time.time()
+    
+    print("::::::::PATH FOUND:::::::::\n\n")
+    print("Printing the path stats now...")
+
+    time.sleep(0.09)
     print(f"Time taken to find shortest path using A* search: {end - start}s")
     print(f"Distance to travel: {distance} units")
     
@@ -73,9 +87,11 @@ def path_finding():
         path_results.append(optimal_path[i].get_dict())
 
     
-    print(f"\ndistance is: {distance}\n")
-    print(f"\npath results are: {path_results}\n")
-    print(f"\ncommands are: {commands}\n")
+    #print(f"\nFinal distance is: {distance}")
+    print(f"Path Results are: {path_results}\n")
+    print(f"\nPath Commands are: {commands}\n")
+
+    print(":::Sending back to RPI:::")
 
 
     return jsonify({
